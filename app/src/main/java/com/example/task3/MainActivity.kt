7509148a7b4012bb0082d8b7adc33bde5fafce3a
Alexic <alexic605@gmail.com>
2021-03-02 17:49:00 +0300

    add value a

---
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.task3
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val a = 4
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
